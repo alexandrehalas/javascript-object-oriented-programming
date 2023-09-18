1b4fae8ca2ced5ad41845b49d449fca771307234
Alexandre Halas <alexandrehalas@gmail.com>
2023-09-18 13:53:30 -0300

    Constructor Functions and the new Operator

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+// CONSTRUCTOR FUNCTIONS AND THE NEW OPERATOR
+const Person = function (firstName, birthYear) {
+  // instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  // instance methods
+  // dont do this
+  //   this.calcAge = function () {
+  //     const currentYear = new Date().getFullYear();
+  //     return currentYear - birthYear;
+  //   };
+};
+
+// 1. New {} object was created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically returns {}
+const alexandre = new Person('Alexandre', '1990');
+const elizabete = new Person('Elizabete', '1986');
+const gabriel = new Person('Gabriel', '2013');
+
+console.log(alexandre);
+console.log(elizabete);
+console.log(gabriel);
+
+console.log(alexandre instanceof Person);
+
+const giulia = { firstName: 'giulia', birthYear: '2000' };
+console.log(giulia instanceof Person);
